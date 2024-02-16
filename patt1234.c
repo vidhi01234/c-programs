e80c7193ce9dce5f40c777a561f6f0017cf5321d
23022673 <160155332+vidhi01234@users.noreply.github.com>
2024-02-16 18:56:43 +0530

    Create patt1234.c

---
+++ patt1234.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("enter the no. of lines");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
