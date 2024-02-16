2a4accd21e9e45e6b4367eecbebd7e5fa7a9d024
23022673 <160155332+vidhi01234@users.noreply.github.com>
2024-02-16 19:39:11 +0530

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10],m,n,i,j;
+    printf("enter the rows and column of matrix");
+    scanf("%d%d",&m,&n);
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
