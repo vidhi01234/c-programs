c24b1de1c0a6a10c7ab1969eb9af152344c4c092
23022673 <160155332+vidhi01234@users.noreply.github.com>
2024-02-28 20:08:26 +0530

    Create sort asc array.c

---
+++ sort asc array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void sort(int*,int);
+int main()
+{
+    int a[10],i,n,*p;
+    printf("enter range");
+    scanf("%d",&n);
+    p=a;
+    puts("enter elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",(p+i));
+    }
+    
+    sort(a,n);
+
+    return 0;
+}
+void sort(int*p,int n)
+{
+    int i,j,t=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+           if(*(p+j)>*(p+j+1)) 
+           {
+               t=*(p+j);
+               *(p+j)=*(p+j+1);
+               *(p+j+1)=t;
+           }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d",*(p+i));
+    }
+    
+}
